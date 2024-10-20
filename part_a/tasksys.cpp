e5f4dc2501ad56a16b47f34ef20cd8d2e7951969
Matthew Jin <mjin.code@gmail.com>
2024-10-20 03:40:10 -0700

    Remove check before queue pop

--- part_a/tasksys.cpp
+++ part_a/tasksys.cpp
@@ -357,7 +357,6 @@ void TaskSystemParallelThreadPoolSleeping::run(IRunnable* runnable, int num_tota
     // method in Parts A and B.  The implementation provided below runs all
     // tasks sequentially on the calling thread.
     //
-
     RunTask first_task = {runnable, 0, num_total_tasks};
 
     pthread_mutex_lock(&_mutex_lock);
@@ -365,9 +364,7 @@ void TaskSystemParallelThreadPoolSleeping::run(IRunnable* runnable, int num_tota
     pthread_cond_broadcast(&_reset);
     pthread_cond_signal(&_queue_add);
     pthread_cond_wait(&_all_threads_done, &_mutex_lock);
-    if (!_work_queue.empty()) {
-        _work_queue.pop();
-    }
+    _work_queue.pop();
     pthread_mutex_unlock(&_mutex_lock);
 }
 
