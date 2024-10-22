8b1f2ea4ff3877d4ffddded31f8a92413a8b5d59
Derek Chong <dc@derekchong.com>
2024-10-22 01:36:45 -0700

    Apparently working part B - sending to server

--- part_b/tasksys.cpp
+++ part_b/tasksys.cpp
@@ -116,11 +116,282 @@ void TaskSystemParallelThreadPoolSpinning::sync() {
     return;
 }
 
+
+
+/*
+ * ================================================================
+ * TaskGraph implementation
+ * ================================================================
+ */
+
+TaskGraph::TaskGraph() {
+    // State variables
+    _task_id_counter = 0;
+    _completed_tasks_counter = 0;
+
+    // Mutex and condition variables
+    pthread_mutex_init(&_tg_lock, NULL);
+    pthread_cond_init(&_task_received, NULL);
+    pthread_cond_init(&_all_tasks_done, NULL);
+
+    // Init data structures
+    _task_graph = std::unordered_map<TaskID, TaskGraphNode>();
+    _ready_tasks = std::deque<TaskID>();
+}
+
+TaskGraph::~TaskGraph() {
+    // printf("TaskGraph::~TaskGraph: Cleaning up\n");
+
+    // Destroy mutex and condition variables
+    pthread_mutex_destroy(&_tg_lock);
+    pthread_cond_destroy(&_task_received);
+    pthread_cond_destroy(&_all_tasks_done);
+}
+
+TaskID TaskGraph::addTask(IRunnable* runnable, int num_total_tasks, const std::vector<TaskID>& deps) {
+    // Adds a new task to the graph, managing graph state and subscribers
+
+    // Prep new task node
+    TaskGraphNode tgn;
+    tgn.work_unit = WorkUnit{0, 0, num_total_tasks, runnable};
+    tgn.n_dependencies = deps.size();
+    tgn.dependents = std::vector<TaskID>();
+    tgn.n_subtasks_done = 0;
+    tgn.done = false;
+
+    // <CRITICAL_SECTION>
+    pthread_mutex_lock(&_tg_lock);
+
+    // Assign ID
+    TaskID cur_tid = _task_id_counter++; // Returns current value, then increments
+    tgn.work_unit.task_id = cur_tid;
+
+    // Add node to graph
+    _task_graph[cur_tid] = tgn;
+
+    // Process task's dependencies
+    for (TaskID dep : deps) {
+        // Add dependent to dependency
+        _task_graph[dep].dependents.push_back(cur_tid);
+
+        // If dependency is done, decrement dependency count
+        if (_task_graph[dep].done) {
+            tgn.n_dependencies--;
+        }
+    }
+
+    // If task has no dependencies, add to ready tasks
+    if (tgn.n_dependencies == 0) {
+        _ready_tasks.push_back(cur_tid);
+    }
+
+    // printf("TaskGraph::addTask: Added task %d: %d tasks, %d deps\n", cur_tid, num_total_tasks, tgn.n_dependencies);
+
+    // Broadcast that a task was added
+    pthread_cond_broadcast(&_task_received);
+
+    pthread_mutex_unlock(&_tg_lock);
+    // </CRITICAL_SECTION>
+
+    return cur_tid;
+}
+
+WorkUnit TaskGraph::markComplete(TaskID task, int subtask_id) {
+    // Receives the completion of a subtask, updates graph state,
+    // and returns the next unit of work to be done, if any
+    // (If no work, task_id will be -1)
+
+    // printf("TaskGraph::markComplete: Task %d, subtask %d\n", task, subtask_id);
+
+    // <CRITICAL_SECTION>
+    pthread_mutex_lock(&_tg_lock);
+
+    // Look up the task graph node
+    TaskGraphNode& tgn = _task_graph[task];
+    WorkUnit wu;
+
+    // Update subtask count
+    tgn.n_subtasks_done++;
+
+    // Was this the last subtask?
+    if (tgn.n_subtasks_done == tgn.work_unit.num_total_tasks) {
+        // Manage task completion
+        tgn.done = true;
+        _completed_tasks_counter++;
+        // printf("TaskGraph::markComplete: Task %d is done\n", task);
+
+        // Process dependents
+        for (TaskID dep : tgn.dependents) {
+            // Decrement dependency count
+            _task_graph[dep].n_dependencies--;
+
+            // printf("TaskGraph::markComplete: Task %d has %d dependencies left\n", dep, _task_graph[dep].n_dependencies);
+
+            // If dependency is done, add to ready tasks
+            if (_task_graph[dep].n_dependencies == 0) {
+                _ready_tasks.push_back(dep);
+                // printf("TaskGraph::markComplete: Task %d is now ready\n", dep);
+            }
+        }
+
+        // Was this the last subtask of the last task?
+        if (_completed_tasks_counter == _task_id_counter) {
+            // Broadcast that all tasks are done
+            pthread_cond_broadcast(&_all_tasks_done);
+        }
+    }
+
+    // Find new work
+    wu = getNextWorkUnitInner();
+
+    pthread_mutex_unlock(&_tg_lock);
+    // </CRITICAL_SECTION>
+
+    return wu;
+}
+
+WorkUnit TaskGraph::getNextWorkUnit() {
+    // Returns the next unit of work to be done, if any
+    // (If no work, task_id will be -1)
+    WorkUnit wu;
+
+    // <CRITICAL_SECTION>
+    pthread_mutex_lock(&_tg_lock);
+
+    while (true) { // Loop to handle spurious wakeups
+        // Run once first for the sync case
+        wu = getNextWorkUnitInner();
+        if (wu.task_id != -1) {
+            // Found work, return it
+            break;
+        } else {
+            // Subscribe to _task_received broadcast, wait for work
+            pthread_cond_wait(&_task_received, &_tg_lock);
+        }
+    }
+
+    pthread_mutex_unlock(&_tg_lock);
+    // </CRITICAL_SECTION>
+
+    assert(wu.task_id != -1);
+    return wu;
+}
+
+WorkUnit TaskGraph::getNextWorkUnitInner() {
+    // Helper function to get the next unit of work to be done, if any
+    // Returns wu with task_id of -1 if no work to do.
+    // Assumes lock is in place.
+
+    WorkUnit wu;
+
+    if (_ready_tasks.empty()) {
+        // No work to do: Return sentinel
+        wu.task_id = -1;
+    } else {
+        // Get next task ID
+        int task_id = _ready_tasks.front();
+        WorkUnit& tg_wu = _task_graph[task_id].work_unit;
+
+        // Setup work unit
+        wu.task_id = task_id;
+        wu.subtask_id = tg_wu.subtask_id;
+        wu.num_total_tasks = tg_wu.num_total_tasks;
+        wu.runnable = tg_wu.runnable;
+
+        // Increment graph subtask_id
+        tg_wu.subtask_id++;
+
+        // Pop task from ready tasks if we just assigned the last subtask
+        if (tg_wu.subtask_id == tg_wu.num_total_tasks) {
+            _ready_tasks.pop_front();
+        }
+    }
+
+    return wu;
+}
+
+void TaskGraph::blockUntilEmpty() {
+    // Called by consumers like sync() to sleep until all tasks are done.
+    // When it returns, we guarantee that all tasks are done.
+
+    // <CRITICAL_SECTION>
+    pthread_mutex_lock(&_tg_lock);
+
+    // Wait for all tasks to be done
+    while (true) { // Loop to handle spurious wakeups
+        if (_completed_tasks_counter == _task_id_counter) {
+            // All tasks are done
+            break;
+        }
+        pthread_cond_wait(&_all_tasks_done, &_tg_lock);
+    }
+
+    pthread_mutex_unlock(&_tg_lock);
+    // </CRITICAL_SECTION>
+}
+
+void TaskGraph::shutdown() {
+    // Called by main to put graph into shutdown state
+    // Ensures queue is empty, then adds sentinel to work queue
+    // and broadcasts to all threads
+
+    // Ensure queue is empty
+    this->blockUntilEmpty();
+
+    // Setup special sentinel work unit
+    // Thread workers exit when they receive this task ID
+    WorkUnit wu_shutdown;
+    wu_shutdown.task_id = -2;
+    wu_shutdown.subtask_id = 0;
+    wu_shutdown.num_total_tasks = 10000;
+
+    // <CRITICAL_SECTION>
+    pthread_mutex_lock(&_tg_lock);
+
+    // Add sentinel to work queue, broadcast to all threads
+    _ready_tasks.push_back(-2);
+    pthread_cond_broadcast(&_task_received);
+
+    pthread_mutex_unlock(&_tg_lock);
+    // </CRITICAL_SECTION>
+}
+
+
+
+
+
+
+
 /*
  * ================================================================
  * Parallel Thread Pool Sleeping Task System Implementation
  * ================================================================
  */
+
+void* threadWorkerB(void *args) {
+    TaskGraph *tg = (TaskGraph *) args;
+    WorkUnit wu;
+    wu.task_id = -1;
+
+    while (true) {
+        if (wu.task_id == -1) {
+            // Get the next work unit
+            wu = tg->getNextWorkUnit();
+        } else if (wu.task_id == -2) {
+            // Received done sentinel, exit
+            // printf("threadWorkerB: Received done sentinel, exiting\n");
+            break;
+        } else {
+            // Run the task and mark it complete
+            wu.runnable->runTask(wu.subtask_id, wu.num_total_tasks);
+            wu = tg->markComplete(wu.task_id, wu.subtask_id);
+        }
+    }
+
+    return NULL;
+}
+
+
 
 const char* TaskSystemParallelThreadPoolSleeping::name() {
     return "Parallel + Thread Pool + Sleep";
@@ -133,6 +404,17 @@ TaskSystemParallelThreadPoolSleeping::TaskSystemParallelThreadPoolSleeping(int n
     // Implementations are free to add new class member variables
     // (requiring changes to tasksys.h).
     //
+
+    // Setup thread pool
+    _num_threads = num_threads;
+    _thread_pool = (pthread_t *) malloc(_num_threads * sizeof(pthread_t));
+
+    // _args = (TaskArgsA3 *) malloc(_num_threads * sizeof(TaskArgsA3));
+
+    // PThread create
+    for (int i = 0; i < _num_threads; i++) {
+        pthread_create(&_thread_pool[i], NULL, threadWorkerB, (void *) &_tg);
+    }
 }
 
 TaskSystemParallelThreadPoolSleeping::~TaskSystemParallelThreadPoolSleeping() {
@@ -142,6 +424,19 @@ TaskSystemParallelThreadPoolSleeping::~TaskSystemParallelThreadPoolSleeping() {
     // Implementations are free to add new class member variables
     // (requiring changes to tasksys.h).
     //
+
+    // Send shutdown signal
+    _tg.shutdown();
+
+    // Join threads
+    for (int i = 0; i < _num_threads; i++) {
+        pthread_join(_thread_pool[i], NULL);
+    }
+
+    // Free memory
+    free(_thread_pool);
+
+    // printf("TaskSystemParallelThreadPoolSleeping::~TaskSystemParallelThreadPoolSleeping: Freeing thread pool\n");
 }
 
 void TaskSystemParallelThreadPoolSleeping::run(IRunnable* runnable, int num_total_tasks) {
@@ -153,9 +448,8 @@ void TaskSystemParallelThreadPoolSleeping::run(IRunnable* runnable, int num_tota
     // tasks sequentially on the calling thread.
     //
 
-    for (int i = 0; i < num_total_tasks; i++) {
-        runnable->runTask(i, num_total_tasks);
-    }
+    _tg.addTask(runnable, num_total_tasks, std::vector<TaskID>());
+    _tg.blockUntilEmpty();
 }
 
 TaskID TaskSystemParallelThreadPoolSleeping::runAsyncWithDeps(IRunnable* runnable, int num_total_tasks,
@@ -166,9 +460,8 @@ TaskID TaskSystemParallelThreadPoolSleeping::runAsyncWithDeps(IRunnable* runnabl
     // TODO: CS149 students will implement this method in Part B.
     //
 
-    for (int i = 0; i < num_total_tasks; i++) {
-        runnable->runTask(i, num_total_tasks);
-    }
+    // Add task to graph
+    _tg.addTask(runnable, num_total_tasks, deps);
 
     return 0;
 }
@@ -178,6 +471,9 @@ void TaskSystemParallelThreadPoolSleeping::sync() {
     //
     // TODO: CS149 students will modify the implementation of this method in Part B.
     //
+
+    // Block until all tasks are done
+    _tg.blockUntilEmpty();
 
     return;
 }
--- part_b/tasksys.h
+++ part_b/tasksys.h
@@ -2,6 +2,9 @@
 #define _TASKSYS_H
 
 #include "itasksys.h"
+#include <unordered_map>
+#include <deque>
+
 
 /*
  * TaskSystemSerial: This class is the student's implementation of a
@@ -53,6 +56,77 @@ class TaskSystemParallelThreadPoolSpinning: public ITaskSystem {
         void sync();
 };
 
+
+
+
+struct WorkUnit {
+    TaskID task_id; // -1 if no work to do
+    int subtask_id; // Invariant: Holds next unassigned subtask_id
+    int num_total_tasks;
+    IRunnable* runnable;
+};
+
+struct TaskGraphNode {
+    WorkUnit work_unit;
+    std::vector<TaskID> dependents; // Tasks that depend on me
+    int n_dependencies; // Tasks that I depend on
+    int n_subtasks_done;
+    bool done;
+};
+
+
+/*
+ * TaskGraph: This class handles the task graph which underlies
+ * TaskSystemParallelThreadPoolSleeping.
+ * 
+ * All public operations acquire _tg_lock and release when done.
+ * Private operations assume lock is in place.
+ */
+class TaskGraph {
+    public:
+        // Basics
+        TaskGraph();
+        ~TaskGraph();
+
+        // Adding always broadcasts
+        TaskID addTask(IRunnable* runnable, int num_total_tasks,
+                                const std::vector<TaskID>& deps);
+
+        // Mark current unit of work done, return next unit of work, if any
+        WorkUnit markComplete(TaskID task, int subtask_id); 
+
+        // Either returns sync or cond_waits
+        WorkUnit getNextWorkUnit(); 
+
+        // Consumers use this to listen for done state (sync)
+        void blockUntilEmpty();
+
+        // Called by main to get all workers to return
+        void shutdown();
+
+    private:
+        int _task_id_counter;
+        int _completed_tasks_counter;
+
+        // Internal graph storage object
+        std::unordered_map<TaskID, TaskGraphNode> _task_graph;
+
+        // Push tasks on back, pop from front
+        // Invariant: Tasks popped as soon as the last subtask is assigned
+        std::deque<TaskID> _ready_tasks; 
+
+        // Assumes lock is in place
+        WorkUnit getNextWorkUnitInner(); 
+
+        // Locks, conditions
+        pthread_mutex_t _tg_lock; // Must have this to update tg in any way
+        pthread_cond_t _task_received; // If there's no work, idle threads subscribe to this and wait for broadcast
+        pthread_cond_t _all_tasks_done; // For sync call
+};
+
+
+void* threadWorkerB(void *args);
+
 /*
  * TaskSystemParallelThreadPoolSleeping: This class is the student's
  * optimized implementation of a parallel task execution engine that uses
@@ -68,6 +142,22 @@ class TaskSystemParallelThreadPoolSleeping: public ITaskSystem {
         TaskID runAsyncWithDeps(IRunnable* runnable, int num_total_tasks,
                                 const std::vector<TaskID>& deps);
         void sync();
+
+    private:
+        TaskGraph _tg;
+        pthread_t * _thread_pool;
+        int _num_threads;
+
+
+
+
+
+
+
+
+
+
+
 };
 
 #endif
